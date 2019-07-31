f5ec12d7cd1ea6ca701557e6c87ee0a69838808e
zuer <1974900537@qq.com>
2019-07-31 23:58:55 +0800

    文件上传下载基本完成

--- src/main/java/com/example/zuer02/controller/UserLoginController.java
+++ src/main/java/com/example/zuer02/controller/UserLoginController.java
@@ -37,6 +37,7 @@ public class UserLoginController {
         Preconditions.checkArgument(password != null&&!"".equals(password), "请输入密码");
         LoginInfo loginInfo=login(new UsernamePasswordToken(String.valueOf(username), String.valueOf(password)));
         System.out.println(loginInfo);
+        System.out.println(JWTUtil.sign(loginInfo.getPrincipal(), loginInfo.getCredential()));
         return JWTUtil.sign(loginInfo.getPrincipal(), loginInfo.getCredential());
     }
 
